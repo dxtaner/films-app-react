1c06a490467ba09e1f52187a863f4e2df67905ad
taner <tanerozer16@gmail.com>
2023-09-16 01:24:53 +0300

    trailerSlice

---
+++ src/app/features/movies/details/trailerSlice.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getMoviesTrailer } from "../../../../Components/Services/movies.js";
+
+const initialState = {
+  trailer: [],
+  status: "boşta",
+};
+
+export const getTrailer = createAsyncThunk(
+  "movies/getMovieTrailer",
+  async (id) => {
+    try {
+      const { results } = await getMoviesTrailer(id);
+      return results;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const trailerSlice = createSlice({
+  name: "trailer",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTrailer.pending, (state) => {
+        state.status = "pending";
+      })
+      .addCase(getTrailer.fulfilled, (state, action) => {
+        state.trailer = action.payload;
+        state.status = "success";
+      });
+  },
+});
+export default trailerSlice.reducer;
+export const trailerList = (state) => state.trailer.trailer;
