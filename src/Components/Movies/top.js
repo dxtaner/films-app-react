e06a80427a75186c83ec756e56dc3d035d56e185
taner <tanerozer16@gmail.com>
2023-09-16 01:25:00 +0300

    TopRatedMovies

---
+++ src/Components/Movies/top.js
@@ -0,0 +1,29 @@
+import MovieCard from "../Cards/Cards.js";
+import { Suspense, useEffect } from "react";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { getTopMovies, topList } from "../../app/features/movies/topSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import { SimpleGrid, Spinner } from "@chakra-ui/react";
+
+const TopRatedMovies = () => {
+  const dispatch = useDispatch();
+  const topRatedList = useSelector(topList);
+
+  useEffect(() => {
+    dispatch(getTopMovies());
+  }, [dispatch]);
+
+  return (
+    <Suspense fallback={<Spinner size="lg" />}>
+      <SimpleGrid
+        justifyItems="center"
+        columns={{ base: 1, sm: 2, md: 3, lg: 5, xl: 6 }}
+        spacing={2}>
+        {topRatedList.map((items, index) => (
+          <MovieCard key={index} movie={items} />
+        ))}
+      </SimpleGrid>
+    </Suspense>
+  );
+};
+export default TopRatedMovies;
