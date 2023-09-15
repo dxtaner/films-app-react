79987586bbc2a105907238216db06f159047820d
taner <tanerozer16@gmail.com>
2023-09-16 01:21:59 +0300

    MenuLinks add

---
+++ src/Components/Header/Navbar/menuLinks.js
@@ -0,0 +1,47 @@
+import { Box, Button, Image, Stack, Text } from "@chakra-ui/react";
+import { MenuItems } from "./menuItems.js";
+import { useNavigate } from "react-router-dom";
+
+export const MenuLinks = ({ isOpen, accountInfo, isAuth }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      display={{ base: isOpen ? "block" : "none", md: "block" }}
+      flexBasis={{ base: "100%", md: "auto" }}>
+      <Stack
+        spacing={8}
+        align="center"
+        justify={["center", "space-between", "flex-end", "flex-end"]}
+        direction={["column", "row", "row", "row"]}
+        pt={[4, 4, 0, 0]}>
+        <MenuItems to="/">
+          <Text>İcons</Text>
+        </MenuItems>
+        <MenuItems to="/estrenos">
+          <Text>Films</Text>
+        </MenuItems>
+        {isAuth ? (
+          <>
+            <MenuItems to="/favorites">
+              <Text>Favorites</Text>
+            </MenuItems>
+            <Image
+              borderRadius="full"
+              src={`https://image.tmdb.org/t/p/original${accountInfo?.avatar_path}`}
+              boxSize="36px"
+            />
+          </>
+        ) : (
+          <Button
+            onClick={() => navigate("/auth/login")}
+            size="sm"
+            bg="red.600"
+            _hover={{ bg: "red.700" }}>
+            Login
+          </Button>
+        )}
+      </Stack>
+    </Box>
+  );
+};
