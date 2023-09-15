ccab04e1d0a67f4d4000d023064d2f043d9cdc31
taner <tanerozer16@gmail.com>
2023-09-16 01:21:27 +0300

    favoritesSlice edited

---
+++ src/app/features/movies/favoritesSlice.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAccountDetails } from "../../../Components/Services/auth.js";
+import { getFavoritesMovies } from "../../../Components/Services/movies.js";
+
+const initialState = {
+  favorites: [],
+  status: "boşta",
+};
+
+export const getFavorites = createAsyncThunk(
+  "movies/getFavorites",
+  async () => {
+    try {
+      const { id } = await getAccountDetails();
+      const { results } = await getFavoritesMovies(id);
+      return results;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getFavorites.pending, (state) => {
+        state.status = "pending";
+      })
+      .addCase(getFavorites.fulfilled, (state, action) => {
+        state.favorites = action.payload;
+        state.status = "success";
+      });
+  },
+});
+export default favoritesSlice.reducer;
+export const favoritesList = (state) => state.favorites.favorites;
