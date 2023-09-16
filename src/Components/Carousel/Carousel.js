8d65c6029ac4cf88c0bfe8f2c624b4fc7d4272af
taner <tanerozer16@gmail.com>
2023-09-16 17:04:25 +0300

    Carousel edited

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,32 +1,36 @@
 import { Carousel } from "react-responsive-carousel";
-// import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Box, useMediaQuery } from "@chakra-ui/react";
+// eslint-disable-next-line no-unused-vars
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./Carousel.css";
 
 const AppCarousel = ({ data }) => {
   const [isMobile] = useMediaQuery("(max-width: 480px)");
 
+  // data.results dizisi tanımlı mı kontrol ediyorum
+  if (!data || !data.results || !Array.isArray(data.results)) {
+    return null; // Veri yoksa veya uygun bir yapıda değilse Carousel'ı render etmiyorum
+  }
+
   return (
     <Carousel showThumbs={false} showArrows autoPlay renderIndicator={false}>
-      {data.map((items, index) => (
-        <div key={index}>
+      {data.results.map((item, index) => (
+        <div className="carousel-item" key={index}>
           <img
-            src={`https://image.tmdb.org/t/p/original${items.backdrop_path}`}
-            alt={items.original_title} // Veya alt=""
+            src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+            alt={item.original_title}
+            className="carousel-image"
           />
-          <Box
-            position="absolute"
-            top="0"
-            width="100%"
-            padding="2"
-            bg="blackAlpha.800"
-            color="white"
-            fontSize={["sm", "md", "lg"]}>
-            {items.original_title}
-          </Box>
-          {!isMobile && <p className="legend">{items.overview}</p>}
+          <div className="carousel-content">
+            <h2 className="carousel-title">{item.original_title}</h2>
+            {!isMobile && (
+              <p className="carousel-description">{item.overview}</p>
+            )}
+          </div>
         </div>
       ))}
     </Carousel>
   );
 };
+
 export default AppCarousel;
