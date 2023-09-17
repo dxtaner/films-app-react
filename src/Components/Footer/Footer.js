73662fe01693869acc8be4ec0d00656482122f0b
taner <tanerozer16@gmail.com>
2023-09-18 01:18:49 +0300

    edited footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,14 @@
-import { Link, Box, Image, Stack, Text, VStack, Icon } from "@chakra-ui/react";
+import {
+  Link,
+  Box,
+  Image,
+  Stack,
+  Text,
+  VStack,
+  Icon,
+  IconButton,
+  Tooltip,
+} from "@chakra-ui/react";
 import Tmdb from "./movie.svg";
 import { FaLinkedin, FaGithub, FaMedium, FaEnvelope } from "react-icons/fa";
 
@@ -11,7 +21,8 @@ const Footer = () => {
       direction={["column", "row", "row", "row"]}
       color="white"
       p="4"
-      borderTop="3px solid red.600">
+      borderTop="3px solid red.600"
+      spacing={4}>
       <Box textAlign="center">
         <Text fontWeight="bold" fontSize="xl" color="blue.300">
           Information
@@ -42,30 +53,51 @@ const Footer = () => {
           Contact
         </Text>
 
-        <Text fontSize="md">
-          <a href="mailto:tanerozer16@gmail.com" className="custom-link">
-            <Icon as={FaEnvelope} w={8} h={8} color="blue.400" />
-          </a>
-        </Text>
         <VStack mt="3" spacing={3} className="link-container">
-          <Link
-            href="https://www.linkedin.com/in/tanerozer16/"
-            target="_blank"
-            className="custom-link">
-            <Icon as={FaLinkedin} w={8} h={8} color="blue.400" />
-          </Link>
-          <Link
-            href="https://github.com/dxtaner"
-            target="_blank"
-            className="custom-link">
-            <Icon as={FaGithub} w={8} h={8} color="blue.400" />
-          </Link>
-          <Link
-            href="https://medium.com/@dxtaner"
-            target="_blank"
-            className="custom-link">
-            <Icon as={FaMedium} w={8} h={8} color="blue.400" />
-          </Link>
+          <Tooltip label="Email">
+            <IconButton
+              as="a"
+              href="mailto:tanerozer16@gmail.com"
+              target="_blank"
+              aria-label="Email"
+              icon={<Icon as={FaEnvelope} w={8} h={8} color="blue.400" />}
+              colorScheme="blue"
+              size="lg"
+            />
+          </Tooltip>
+          <Tooltip label="LinkedIn">
+            <IconButton
+              as="a"
+              href="https://www.linkedin.com/in/tanerozer16/"
+              target="_blank"
+              aria-label="LinkedIn"
+              icon={<Icon as={FaLinkedin} w={8} h={8} color="blue.400" />}
+              colorScheme="blue"
+              size="lg"
+            />
+          </Tooltip>
+          <Tooltip label="GitHub">
+            <IconButton
+              as="a"
+              href="https://github.com/dxtaner"
+              target="_blank"
+              aria-label="GitHub"
+              icon={<Icon as={FaGithub} w={8} h={8} color="blue.400" />}
+              colorScheme="blue"
+              size="lg"
+            />
+          </Tooltip>
+          <Tooltip label="Medium">
+            <IconButton
+              as="a"
+              href="https://medium.com/@dxtaner"
+              target="_blank"
+              aria-label="Medium"
+              icon={<Icon as={FaMedium} w={8} h={8} color="blue.400" />}
+              colorScheme="blue"
+              size="lg"
+            />
+          </Tooltip>
         </VStack>
       </Box>
     </Stack>
