244ac5f3a2cf5484b15ca321e8f9851906c31c44
taner <tanerozer16@gmail.com>
2023-09-17 20:36:56 +0300

    Edited Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./Components/Home/Home.js";
-import Details from "./Components/Movies/Details.js";
+import About from "./Components/Home/About.js";
+import MovieDetails from "./Components/Movies/DetailsMovie.js";
 import LoginForm from "./Components/Auth/LoginForm.js";
-import Favorites from "./Components/Movies/Favorites.js";
-import UpcomingMovies from "./Components/Movies/Upcoming.js";
+import Favorites from "./Components/Movies/FavoriteMovies.js";
+import UpcomingMovies from "./Components/Movies/UpcomingMovies.js";
 import NavBar from "./Components/Header/Navbar/index.js";
 import Footer from "./Components/Footer/Footer.js";
+import ActorDetails from "./Components/Actor/ActorDetails.js";
+import PersonPopular from "./Components/Actor/PopularPerson.js";
 
 function App() {
   return (
@@ -14,10 +17,13 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<UpcomingMovies />} />
-        <Route path="/details/:id" element={<Details />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/upComingMovies" element={<UpcomingMovies />} />
+        <Route path="/movieDetails/:id" element={<MovieDetails />} />
         <Route path="/auth/login" element={<LoginForm />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/actorDetails/:id" element={<ActorDetails />} />
+        <Route path="/popularPersons" element={<PersonPopular />} />
       </Routes>
       <Footer />
     </Router>
