6e1fd8a80fb65f07128d49665285d201b11b9f48
taner <tanerozer16@gmail.com>
2023-09-18 19:20:15 +0300

    UpcomingMovies edited

--- src/Components/Movies/UpcomingMovies.js
+++ src/Components/Movies/UpcomingMovies.js
@@ -1,9 +1,18 @@
 import { useEffect } from "react";
-import { SimpleGrid, VStack, Text, StackDivider, Box } from "@chakra-ui/react";
-import MovieCard from "../Cards/Cards.js";
+import {
+  SimpleGrid,
+  VStack,
+  Text,
+  StackDivider,
+  Box,
+  Center,
+  Spinner,
+} from "@chakra-ui/react";
+import MovieCard from "../Cards/MovieCards.js";
 import {
   getUpcoming,
   upcomingList,
+  upcomingLoading,
 } from "../../app/features/movies/upcomingSlice.js";
 import { useDispatch, useSelector } from "react-redux";
 import Title from "../Title/titles.js";
@@ -11,6 +20,7 @@ import Title from "../Title/titles.js";
 const UpcomingMovies = () => {
   const dispatch = useDispatch();
   const upcomingMovies = useSelector(upcomingList);
+  const isLoading = useSelector(upcomingLoading);
 
   useEffect(() => {
     dispatch(getUpcoming());
@@ -22,18 +32,24 @@ const UpcomingMovies = () => {
       spacing={4}
       p={[2, 4, 6, 8]}>
       <Box>
-        <Title text="Yakşalan ve Vizyondaki Filmler">
+        <Title text="Yaklaşan ve Vizyondaki Filmler">
           <Text>Yaklaşan ve Vizyondaki tüm Filmlerin listesi</Text>
         </Title>
       </Box>
-      <SimpleGrid
-        justifyItems="center"
-        columns={{ base: 1, sm: 2, md: 3, lg: 5, xl: 6 }}
-        spacing={2}>
-        {upcomingMovies.map((items) => (
-          <MovieCard key={items.id} movie={items} />
-        ))}
-      </SimpleGrid>
+      {isLoading ? (
+        <Center>
+          <Spinner size="xl" />
+        </Center>
+      ) : (
+        <SimpleGrid
+          justifyItems="center"
+          columns={{ base: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
+          spacing={4}>
+          {upcomingMovies.map((item) => (
+            <MovieCard key={item.id} movie={item} />
+          ))}
+        </SimpleGrid>
+      )}
     </VStack>
   );
 };
