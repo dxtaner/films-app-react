21b554adec958d3c0c58c53e08b864a36abb9fab
taner <tanerozer16@gmail.com>
2023-09-18 04:14:32 +0300

    Favorites edited

--- src/Components/Movies/FavoriteMovies.js
+++ src/Components/Movies/FavoriteMovies.js
@@ -1,17 +1,27 @@
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import MovieCard from "../Cards/Cards.js";
-import { Box, VStack, SimpleGrid, StackDivider, Text } from "@chakra-ui/react";
+import MovieCard from "../Cards/MovieCards.js";
+import {
+  Box,
+  VStack,
+  SimpleGrid,
+  StackDivider,
+  Text,
+  Center,
+  Spinner,
+} from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getFavorites,
   favoritesList,
+  favoritesLoading,
 } from "../../app/features/movies/favoritesSlice.js";
 import Title from "../Title/titles.js";
 
 const Favorites = () => {
   const dispatch = useDispatch();
   const favorites = useSelector(favoritesList);
+  const isLoading = useSelector(favoritesLoading);
   const token = sessionStorage.getItem("session_id");
 
   useEffect(() => {
@@ -24,25 +34,30 @@ const Favorites = () => {
     <VStack
       divider={<StackDivider borderColor="blue.800" />}
       justifyContent="center"
-      p={[2, 4, 6, 8]}>
+      p={[2, 4, 6, 8]}
+      spacing={4}>
       {!token && <Navigate replace to="/auth/login" />}
       <Box>
         <Title text="Favori Filmlerim">
           Buradaki filmlerin tümü hesabınızla ilişkili favori filmlerinizdir.
         </Title>
       </Box>
-      <SimpleGrid
-        mt="4"
-        columns={{ base: 1, sm: 2, md: 3, lg: 5, xl: 6 }}
-        spacing={2}>
-        {favorites.length > 0 ? (
-          favorites.map((items, index) => (
-            <MovieCard key={index} movie={items} />
-          ))
-        ) : (
-          <Text>Favori filminiz yok</Text>
-        )}
-      </SimpleGrid>
+      {isLoading ? (
+        <Center>
+          <Spinner size="xl" />
+        </Center>
+      ) : (
+        <SimpleGrid
+          mt="4"
+          columns={{ base: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
+          spacing={4}>
+          {favorites.length > 0 ? (
+            favorites.map((item) => <MovieCard key={item.id} movie={item} />)
+          ) : (
+            <Text>Favori filminiz yok</Text>
+          )}
+        </SimpleGrid>
+      )}
     </VStack>
   );
 };
