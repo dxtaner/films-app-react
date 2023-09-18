55c929eccf7ef004f08eaabba7def46ed34d04d9
taner <tanerozer16@gmail.com>
2023-09-19 00:39:08 +0300

    MovieImage added

---
+++ src/Components/Movies/MovieImage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+
+const MovieImage = ({ imageUrl, altText }) => {
+  return (
+    <Box
+      cursor="pointer"
+      w="100%"
+      maxW="800px"
+      mx="auto"
+      m={2}
+      borderRadius="lg"
+      overflow="hidden"
+      transition="transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out"
+      _hover={{
+        transform: "scale(1.05)",
+        boxShadow: "lg",
+      }}
+      borderWidth="1px"
+      borderColor="gray.200">
+      <Image
+        src={imageUrl}
+        alt={altText}
+        w="100%"
+        h="auto"
+        objectFit="cover"
+        objectPosition="center top"
+      />
+    </Box>
+  );
+};
+
+export default MovieImage;
