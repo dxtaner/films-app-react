87f51afb77b00a6efa236f90e64300673daa0d24
taner <tanerozer16@gmail.com>
2023-09-18 04:12:12 +0300

    edited Links

--- src/Components/Header/Navbar/menuItems.js
+++ src/Components/Header/Navbar/menuItems.js
@@ -4,8 +4,8 @@ import { Link as ReachLink } from "react-router-dom";
 export const MenuItems = ({ children, to = "/" }) => {
   return (
     <Link
-      _hover={{ textDecor: "none", color: "red.500" }}
-      _focus={{ color: "red.500" }}
+      _hover={{ textDecor: "none", color: "gray.400" }}
+      _focus={{ color: "gray.500" }}
       as={ReachLink}
       to={to}>
       {children}
