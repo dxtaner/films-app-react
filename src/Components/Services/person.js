f4214c882a632f5d16da05c91f3c62865a52d910
taner <tanerozer16@gmail.com>
2023-09-18 19:19:55 +0300

    person services added

---
+++ src/Components/Services/person.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+// import { showErrorMessage } from "../Alerts.js";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_KEY = process.env.REACT_APP_APIKEY;
+
+export const getPopularPersons = () => {
+  return axios
+    .get(`${BASE_URL}/person/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+};
+
+export const getPersonInfo = (id) => {
+  const requestURL = `${BASE_URL}/person/${id}?api_key=${API_KEY}&language=en-US`;
+
+  // console.log("Person ID:", id); // Kişi kimliğini konsola yazdır
+  // console.log("Request URL:", requestURL); // İstek URL'sini konsola yazdır
+
+  return axios
+    .get(requestURL)
+    .then((response) => {
+      console.log("Response Data:", response.data); // API yanıtını konsola yazdır
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(`Kişi bilgileri alınamadı (ID: ${id}): `, error);
+      throw error;
+    });
+};
+
+export const getPersonMovieCredits = (personId) => {
+  return axios
+    .get(
+      `${BASE_URL}/person/${personId}/movie_credits?api_key=${API_KEY}&language=en-US`
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error("Kişi film kredileri alınamadı: ", error);
+      throw error;
+    });
+};
