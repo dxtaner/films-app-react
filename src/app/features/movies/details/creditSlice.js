f407bef5c74e0478b5ef50075d3ef5145ae383c4
taner <tanerozer16@gmail.com>
2023-09-18 04:11:09 +0300

    creditSlice added

---
+++ src/app/features/movies/details/creditSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getMoviesCredit } from "../../../../Components/Services/movies.js";
+
+const initialState = {
+  credit: [],
+  status: "idle",
+  error: null,
+};
+
+export const getCredit = createAsyncThunk(
+  "movies/getMovieCredit",
+  async (id, { rejectWithValue }) => {
+    try {
+      const results = await getMoviesCredit(id);
+      return results.cast;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const creditSlice = createSlice({
+  name: "credit",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCredit.pending, (state) => {
+        state.status = "pending";
+      })
+      .addCase(getCredit.fulfilled, (state, action) => {
+        state.credit = action.payload;
+        state.status = "success";
+      });
+  },
+});
+
+export default creditSlice.reducer;
+export const creditList = (state) => state.credit.credit;
