4fd1621147c14f2e52a03283a2d9d448b01652d3
taner <tanerozer16@gmail.com>
2023-09-21 02:06:56 +0300

    ratedMovieSlice added

---
+++ src/app/features/movies/ratedMoiveSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getRatedMovies } from "../../../Components/Services/movies.js";
+
+const initialState = {
+  ratedMovies: [],
+  isLoading: false,
+};
+
+// Derecelendirilmiş filmleri alma işlemi
+export const fetchRatedMovies = createAsyncThunk(
+  "ratedMovies/fetchRatedMovies",
+  async (account_id, thunkAPI) => {
+    try {
+      const ratedMovies = await getRatedMovies(account_id);
+      return ratedMovies;
+    } catch (error) {
+      console.error("Derecelendirilmiş filmleri alma işleminde hata:", error);
+      throw error;
+    }
+  }
+);
+
+export const ratedMovieSlice = createSlice({
+  name: "ratedMovies",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRatedMovies.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchRatedMovies.fulfilled, (state, action) => {
+        state.ratedMovies = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchRatedMovies.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export default ratedMovieSlice.reducer;
+export const selectRatedMovies = (state) => state.ratedMovies.ratedMovies;
+export const selectRatedMoviesLoading = (state) => state.ratedMovies.isLoading;
