3886ce69145eac5368fa29bd7889f28c6152cba9
taner <tanerozer16@gmail.com>
2023-09-21 03:52:40 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./Components/Home/Home.js";
 import About from "./Components/Home/About.js";
 import MovieDetails from "./Components/Movies/Details.js";
 import LoginForm from "./Components/Auth/LoginForm.js";
-import Favorites from "./Components/Movies/FavoriteMovies.js";
+import FavoriteMovies from "./Components/Movies/FavoriteMovies.js";
 import UpcomingMovies from "./Components/Movies/UpcomingMovies.js";
 import NavBar from "./Components/Header/Navbar/index.js";
 import Footer from "./Components/Footer/Footer.js";
@@ -24,7 +24,7 @@ function App() {
         <Route path="/upComingMovies" element={<UpcomingMovies />} />
         <Route path="/movieDetails/:id" element={<MovieDetails />} />
         <Route path="/auth/login" element={<LoginForm />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/myFavoriteMovives" element={<FavoriteMovies />} />
         <Route path="/watchListMovies" element={<WatchListMovies />} />
         <Route path="/actorDetails/:id" element={<ActorDetails />} />
         <Route path="/popularPersons" element={<PersonPopular />} />
