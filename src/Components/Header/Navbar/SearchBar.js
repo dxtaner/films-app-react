44d591b4d1693505afaa1e4f93ef0fc76f61a00d
taner <tanerozer16@gmail.com>
2023-09-21 19:43:22 +0300

    SearchBar added

---
+++ src/Components/Header/Navbar/SearchBar.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Input } from "@chakra-ui/react";
+import { searchMoviesAsync } from "../../../app/features/movies/searchSlice.js";
+import { useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const SearchBar = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    dispatch(searchMoviesAsync(location.state?.id));
+  }, [dispatch, location.state]);
+
+  useEffect(() => {
+    setQuery("");
+  }, [location.key]);
+
+  const handleSearch = () => {
+    dispatch(searchMoviesAsync(query));
+    navigate(`/searchMovies?query=${query}`);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <Input
+        placeholder="Film Arayın.."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
