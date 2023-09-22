72367a68d6bda4876d7cc590efaf74a08a243279
taner <tanerozer16@gmail.com>
2023-09-23 00:22:24 +0300

    PrimaryReleaseYearFilter added

---
+++ src/Components/Movies/FilteredMovies/PrimaryReleaseYearFilter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Input } from "@chakra-ui/react";
+
+const PrimaryReleaseYearFilter = ({ queryParams, onFilterChange }) => {
+  const handleYearChange = (e) => {
+    const year = e.target.value;
+    onFilterChange("primary_release_year", year);
+  };
+
+  return (
+    <Input
+      type="number"
+      value={queryParams.primary_release_year || ""}
+      onChange={handleYearChange}
+      size="xs" // Küçük boyutta giriş alanı
+      borderWidth="1px" // İnce kenarlık
+      borderRadius="sm" // Hafif yuvarlak kenarlar
+      borderColor="gray.300" // Kenarlık rengi
+      _focus={{ borderColor: "blue.400", boxShadow: "none" }} // Odaklandığında stillendirme
+      maxWidth="100px" // Maksimum genişlik
+      placeholder="Yıl"
+    />
+  );
+};
+
+export default PrimaryReleaseYearFilter;
