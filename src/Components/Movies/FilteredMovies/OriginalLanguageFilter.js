74f0e8d258e66a8499ce95ed1c7b6a121b7f687a
taner <tanerozer16@gmail.com>
2023-09-22 23:49:01 +0300

    OriginalLanguageFilter added

---
+++ src/Components/Movies/FilteredMovies/OriginalLanguageFilter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FaLanguage } from "react-icons/fa";
+
+const OriginalLanguageFilter = ({ queryParams, onFilterChange }) => {
+  return (
+    <InputGroup size="sm">
+      <InputLeftElement pointerEvents="none">
+        <FaLanguage color="gray.300" />
+      </InputLeftElement>
+      <Input
+        type="text"
+        value={queryParams.with_original_language || ""}
+        onChange={(e) =>
+          onFilterChange("with_original_language", e.target.value)
+        }
+        placeholder="Orijinal Dil"
+      />
+    </InputGroup>
+  );
+};
+
+export default OriginalLanguageFilter;
