8f42f97cfa153b6924c4f819e202148639f2c39f
taner <tanerozer16@gmail.com>
2023-09-23 02:04:35 +0300

    edite LoadMore

--- src/Components/Movies/LoadMoreButton.js
+++ src/Components/Movies/LoadMoreButton.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Button, Center, Spinner } from "@chakra-ui/react";
 
-const LoadMoreButton = ({ onClick }) => {
+const LoadMoreButton = ({ onClick, status }) => {
+  if (status === "loading") {
+    return (
+      <Center mt={6}>
+        <Spinner size="xl" color="teal.500" />
+      </Center>
+    );
+  }
   return (
     <Button onClick={onClick} marginTop={4} colorScheme="teal">
       Daha Fazla Yükle
