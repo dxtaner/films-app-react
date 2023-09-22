5ae5332bd11ea18666857cb80440dc4bb4d0fe74
taner <tanerozer16@gmail.com>
2023-09-23 00:35:40 +0300

    EndDateFilter added

---
+++ src/Components/Movies/FilteredMovies/EndDateFilter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Input, Text } from "@chakra-ui/react";
+
+const EndDateFilter = ({ queryParams, onFilterChange }) => {
+  const handleEndDateChange = (e) => {
+    onFilterChange("primary_release_date.lte", e.target.value);
+  };
+
+  return (
+    <div>
+      <Text fontSize="sm" fontWeight="semibold">
+        Bitiş Tarihi
+      </Text>
+      <Input
+        type="date"
+        value={queryParams.primary_release_date.lte || ""}
+        onChange={handleEndDateChange}
+        size="xs"
+        borderWidth="1px"
+        borderRadius="sm"
+        borderColor="gray.300"
+        _focus={{ borderColor: "blue.400", boxShadow: "none" }}
+        maxWidth="100px"
+      />
+    </div>
+  );
+};
+
+export default EndDateFilter;
