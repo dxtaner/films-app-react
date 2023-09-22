ab663115cb0d1a2499c1a65273737e19c099f6a9
taner <tanerozer16@gmail.com>
2023-09-23 00:22:17 +0300

    MaxRatingSlider added

---
+++ src/Components/Movies/FilteredMovies/MaxRatingSlider.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Text,
+} from "@chakra-ui/react";
+
+const MaxRatingSlider = ({ queryParams, onFilterChange }) => {
+  const handleMaxRatingChange = (value) => {
+    onFilterChange("vote_average.lte", value.toFixed(1));
+  };
+
+  return (
+    <div>
+      <Slider
+        min={0}
+        max={10}
+        step={0.1}
+        value={parseFloat(queryParams["vote_average.lte"] || 10)}
+        onChange={handleMaxRatingChange}
+        size="xs" // Küçük boyut
+        colorScheme="blue" // Renk şeması
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb boxSize={4} bg="blue.400" />
+      </Slider>
+      <Text mt={2} fontSize="xs" textAlign="center">
+        Seçilen Değer:{" "}
+        {parseFloat(queryParams["vote_average.lte"] || 10).toFixed(1)}
+      </Text>
+    </div>
+  );
+};
+
+export default MaxRatingSlider;
