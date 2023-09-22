e95c4c7bf73c2147dc1eb1d159940c0586743588
taner <tanerozer16@gmail.com>
2023-09-22 23:49:07 +0300

    YearFilter added

---
+++ src/Components/Movies/FilteredMovies/YearFilter.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FaCalendar } from "react-icons/fa";
+
+const YearFilter = ({ queryParams, onFilterChange }) => {
+  return (
+    <InputGroup size="xs">
+      <InputLeftElement pointerEvents="none">
+        <FaCalendar color="gray.400" />
+      </InputLeftElement>
+      <Input
+        type="number"
+        value={queryParams.year || ""}
+        onChange={(e) => onFilterChange("year", e.target.value)}
+        placeholder="Yıl"
+        size="xs"
+        fontSize="sm"
+        border="1px solid"
+        borderColor="gray.200"
+        borderRadius="md"
+        _hover={{
+          borderColor: "gray.300",
+        }}
+        _focus={{
+          borderColor: "blue.400",
+          boxShadow: "none",
+        }}
+      />
+    </InputGroup>
+  );
+};
+
+export default YearFilter;
