969c406dfaa4619a0a46c3571ddc520a095fd51a
taner <tanerozer16@gmail.com>
2023-09-23 19:50:07 +0300

    Remove from Favorites and  Add to Favorites and  Remove from Watchlist and Add to WatchList added

--- src/Components/Cards/MovieCards.js
+++ src/Components/Cards/MovieCards.js
@@ -1,16 +1,90 @@
-import React from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Image,
+  Text,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Divider,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+import { MdMoreVert } from "react-icons/md";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSignInAlt,
+  faHeart,
+  faHeartBroken,
+  faPlusCircle,
+  faMinusCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import nullImage from "../NullImage/null.png";
+import {
+  getFavorites,
+  favoritesList,
+} from "../../app/features/movies/favoritesSlice.js";
+import {
+  getWatchList,
+  watchListMovies,
+} from "../../app/features/movies/watchListSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToFavorites,
+  removeFromFavorites,
+  addToWatchList,
+  remevoToWatchList,
+} from "../../app/features/movies/details/detailsSlice";
 
 const MotionBox = motion.div;
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const favorites = useSelector(favoritesList);
+  const watchList = useSelector(watchListMovies);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isWatchlist, setIsWatchlist] = useState(false);
+  const isAuth = sessionStorage.getItem("session_id");
+  const token = sessionStorage.getItem("session_id");
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getFavorites());
+      dispatch(getWatchList());
+    }
+  }, [dispatch, token, movie.id]);
+
+  const favoriteIDs = favorites.map((favorite) => favorite.id);
+  const watchListIDs = watchList.map((watch) => watch.id);
+  // console.log("favorites uzln", favorites.length);
+  // console.log("watch uzln", watchList.length);
 
   const showDetails = () => {
     navigate(`/movieDetails/${movie.id}`, { state: movie });
+  };
+
+  const addToToggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+    dispatch(addToFavorites(movie.id, true));
+  };
+
+  const removeToToggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+    dispatch(removeFromFavorites(movie.id, true));
+  };
+
+  const addToToggleWatchlist = () => {
+    setIsWatchlist(!isWatchlist);
+    dispatch(addToWatchList(movie.id, true));
+  };
+
+  const removeToToggleWatchlist = () => {
+    setIsWatchlist(!isWatchlist);
+    dispatch(remevoToWatchList(movie.id, true));
   };
 
   const { poster_path, release_date, vote_average } = movie;
@@ -68,6 +142,99 @@ const MovieCard = ({ movie }) => {
             zIndex="1">
             {vote_average.toFixed(1) * 10}%
           </Text>
+          <Menu
+            isOpen={isMenuOpen}
+            onOpen={() => setIsMenuOpen(true)}
+            onClose={() => setIsMenuOpen(false)}>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<MdMoreVert />}
+              position="absolute"
+              top={2}
+              right={2}
+              zIndex="2"
+              size="sm"
+            />
+            <MenuList fontSize="sm">
+              {!isAuth ? (
+                <MenuItem size="xs" onClick={() => navigate("/auth/login")}>
+                  <FontAwesomeIcon icon={faSignInAlt} /> Giriş Yap
+                </MenuItem>
+              ) : (
+                <>
+                  {favoriteIDs.includes(movie.id) ? (
+                    <MenuItem
+                      size="xs"
+                      onClick={removeToToggleFavorite}
+                      icon={
+                        <Box
+                          as={FontAwesomeIcon}
+                          icon={faHeartBroken}
+                          fontSize="1.5rem"
+                          marginRight="0.5rem"
+                        />
+                      }>
+                      <Text fontWeight="bold" fontSize="sm">
+                        Remove from Favorites
+                      </Text>
+                    </MenuItem>
+                  ) : (
+                    <MenuItem
+                      size="xs"
+                      onClick={addToToggleFavorite}
+                      icon={
+                        <Box
+                          as={FontAwesomeIcon}
+                          icon={faHeart}
+                          fontSize="1.5rem"
+                          marginRight="0.5rem"
+                        />
+                      }>
+                      <Text fontWeight="bold" fontSize="sm">
+                        Add to Favorites
+                      </Text>
+                    </MenuItem>
+                  )}
+
+                  <Divider />
+                  {watchListIDs.includes(movie.id) ? (
+                    <MenuItem
+                      size="xs"
+                      onClick={removeToToggleWatchlist}
+                      icon={
+                        <Box
+                          as={FontAwesomeIcon}
+                          icon={faMinusCircle}
+                          fontSize="1.5rem"
+                          marginRight="0.5rem"
+                        />
+                      }>
+                      <Text fontWeight="bold" fontSize="sm">
+                        Remove from Watchlist
+                      </Text>
+                    </MenuItem>
+                  ) : (
+                    <MenuItem
+                      size="xs"
+                      onClick={addToToggleWatchlist}
+                      icon={
+                        <Box
+                          as={FontAwesomeIcon}
+                          icon={faPlusCircle}
+                          fontSize="1.5rem"
+                          marginRight="0.5rem"
+                        />
+                      }>
+                      <Text fontWeight="bold" fontSize="sm">
+                        Add to WatchList
+                      </Text>
+                    </MenuItem>
+                  )}
+                </>
+              )}
+            </MenuList>
+          </Menu>
         </Box>
       </MotionBox>
       <Box p={2}>
