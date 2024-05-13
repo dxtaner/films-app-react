4a71934b47b95b4beccc9b287065142c046b1aae
taner <tanerozer16@gmail.com>
2024-05-13 16:16:25 +0300

    SearchBar edited

--- src/Components/Header/Navbar/SearchBar.js
+++ src/Components/Header/Navbar/SearchBar.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Input } from "@chakra-ui/react";
+import { Input, Box } from "@chakra-ui/react";
 import { searchMoviesAsync } from "../../../app/features/movies/searchSlice.js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
+import SearchResults from "./SearchResults";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
@@ -10,6 +11,7 @@ const SearchBar = () => {
   const navigate = useNavigate();
 
   const [query, setQuery] = useState("");
+  const searchResults = useSelector((state) => state.search.results);
 
   useEffect(() => {
     if (location.state && location.state.id) {
@@ -21,10 +23,11 @@ const SearchBar = () => {
     setQuery("");
   }, [location.key]);
 
-  const handleSearch = () => {
-    dispatch(searchMoviesAsync(query));
-    navigate(`/searchMovies?query=${query}`);
-  };
+  useEffect(() => {
+    if (query.trim() !== "") {
+      dispatch(searchMoviesAsync(query));
+    }
+  }, [dispatch, navigate, query]);
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -32,15 +35,44 @@ const SearchBar = () => {
     }
   };
 
+  const handleResultClick = (result) => {
+    console.log("Selected Movie:", result);
+  };
+
+  const handleSearch = () => {
+    if (query.trim() !== "") {
+      dispatch(searchMoviesAsync(query));
+      navigate(`/searchMovies?query=${query}`);
+    }
+  };
+
   return (
-    <div>
-      <Input
-        placeholder="Film Arayın.."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={handleKeyPress}
-      />
-    </div>
+    <Box display="flex" justifyContent="center" margin="20px">
+      <Box
+        position="relative"
+        width="1000px"
+        bg="white"
+        borderRadius="xl"
+        boxShadow="md"
+        p="4">
+        <Input
+          placeholder="Film Arayın.."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={handleKeyPress}
+          borderRadius="xl"
+          borderColor="blue.300"
+          _hover={{ borderColor: "blue.500" }}
+          _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
+        />
+        {query && (
+          <SearchResults
+            results={searchResults}
+            handleResultClick={handleResultClick}
+          />
+        )}
+      </Box>
+    </Box>
   );
 };
 
