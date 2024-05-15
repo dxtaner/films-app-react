b0533530482aab186c2ed6782b43e898a4c697f4
taner <tanerozer16@gmail.com>
2024-05-15 19:49:00 +0300

    SocialIdentityLinks edited

--- src/Components/Actor/SocialIdentityLinks.js
+++ src/Components/Actor/SocialIdentityLinks.js
@@ -1,84 +1,83 @@
-// components/SocialIdentityLinks.js
-
 import React from "react";
-import { Flex, Icon } from "@chakra-ui/react";
+import { Flex, Icon, Link, Tooltip } from "@chakra-ui/react";
 import {
   FaFacebook,
   FaImdb,
   FaInstagram,
+  FaTiktok,
   FaTwitter,
   FaWikipediaW,
+  FaYoutubeSquare,
 } from "react-icons/fa";
 
 const SocialIdentityLinks = ({ externalIds }) => {
+  const socialLinks = [
+    {
+      name: "IMDB",
+      icon: FaImdb,
+      id: "imdb_id",
+      url: `https://www.imdb.com/name/${externalIds?.imdb_id}`,
+    },
+    {
+      name: "WikiData",
+      icon: FaWikipediaW,
+      id: "wikidata_id",
+      url: `https://www.wikidata.org/wiki/${externalIds?.wikidata_id}`,
+    },
+    {
+      name: "Facebook",
+      icon: FaFacebook,
+      id: "facebook_id",
+      url: `https://www.facebook.com/${externalIds?.facebook_id}`,
+    },
+    {
+      name: "Twitter",
+      icon: FaTwitter,
+      id: "twitter_id",
+      url: `https://twitter.com/${externalIds?.twitter_id}`,
+    },
+    {
+      name: "Instagram",
+      icon: FaInstagram,
+      id: "instagram_id",
+      url: `https://www.instagram.com/${externalIds?.instagram_id}`,
+    },
+    {
+      name: "TikTok",
+      icon: FaTiktok,
+      id: "tiktok_id",
+      url: `https://www.tiktok.com/${externalIds?.tiktok_id}`,
+    },
+    {
+      name: "YouTube",
+      icon: FaYoutubeSquare,
+      id: "youtube_id",
+      url: `https://www.youtube.com/${externalIds?.youtube_id}`,
+    },
+  ];
+
   return (
-    <Flex alignItems="center" mt={2} mb={2}>
+    <Flex as="section" alignItems="center" mt={4} mb={2}>
       <strong>Sosyal Kimlik Bilgileri:</strong>
-      <ul
-        style={{
-          listStyleType: "none",
-          paddingInlineStart: 0,
-          display: "flex",
-          gap: "10px",
-          margin: "5px",
-        }}>
-        {externalIds && externalIds.imdb_id && (
-          <li>
-            <a
-              href={`https://www.imdb.com/name/${externalIds.imdb_id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "#333" }}>
-              <Icon as={FaImdb} boxSize={5} /> IMDB
-            </a>
-          </li>
-        )}
-        {externalIds && externalIds.wikidata_id && (
-          <li>
-            <a
-              href={`https://www.wikidata.org/wiki/${externalIds.wikidata_id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "#333" }}>
-              <Icon as={FaWikipediaW} boxSize={5} /> WikiData
-            </a>
-          </li>
-        )}
-        {externalIds && externalIds.facebook_id && (
-          <li>
-            <a
-              href={`https://www.facebook.com/${externalIds.facebook_id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "#333" }}>
-              <Icon as={FaFacebook} boxSize={5} /> Facebook
-            </a>
-          </li>
-        )}
-        {externalIds && externalIds.twitter_id && (
-          <li>
-            <a
-              href={`https://twitter.com/${externalIds.twitter_id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "#333" }}>
-              <Icon as={FaTwitter} boxSize={5} /> Twitter
-            </a>
-          </li>
-        )}
-        {externalIds && externalIds.instagram_id && (
-          <li>
-            <a
-              href={`https://www.instagram.com/${externalIds.instagram_id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "#333" }}>
-              <Icon as={FaInstagram} boxSize={5} /> Instagram
-            </a>
-          </li>
-        )}
-      </ul>
+      <Flex ml={4} wrap="wrap" justify="flex-start">
+        {socialLinks.map((link, index) => (
+          <Tooltip key={index} label={link.name} aria-label={link.name}>
+            <Link
+              key={index}
+              href={link.url}
+              isExternal
+              textDecoration="none"
+              fontSize="xl"
+              color="gray.600"
+              _hover={{ color: "blue.500", transform: "scale(1.1)" }}
+              mr={4}>
+              <Icon as={link.icon} boxSize={8} />
+            </Link>
+          </Tooltip>
+        ))}
+      </Flex>
     </Flex>
   );
 };
+
 export default SocialIdentityLinks;
