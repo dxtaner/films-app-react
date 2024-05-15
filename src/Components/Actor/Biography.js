c9a8566d6a636b781d5cdd858440ae5477f3de35
taner <tanerozer16@gmail.com>
2024-05-15 18:28:47 +0300

    Biography edited

--- src/Components/Actor/Biography.js
+++ src/Components/Actor/Biography.js
@@ -1,16 +1,18 @@
-// components/Biography.js
-
 import React from "react";
-import { Text, VStack, Heading } from "@chakra-ui/react";
+import { Text, VStack, Heading, Box } from "@chakra-ui/react";
 
 function Biography({ biography }) {
   return (
-    <VStack mt={4} align="start">
-      <Heading as="h2" size="lg" mb={2}>
-        Biyografi
-      </Heading>
-      <Text fontSize="lg">{biography}</Text>
-    </VStack>
+    <Box p={4} borderRadius="lg" bg="gray.100">
+      <VStack spacing={4} align="start">
+        <Heading as="h2" size="lg" mb={2}>
+          Biyografi
+        </Heading>
+        <Text fontSize="lg" color="gray.600">
+          {biography}
+        </Text>
+      </VStack>
+    </Box>
   );
 }
 
