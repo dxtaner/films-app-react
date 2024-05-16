709d57c143eedfffc4e4f1fe7cedde4273918d57
taner <tanerozer16@gmail.com>
2024-05-16 15:45:29 +0300

    ActorDetails edited

--- src/Components/Actor/ActorDetails.js
+++ src/Components/Actor/ActorDetails.js
@@ -1,78 +1,86 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Box, VStack, Spinner, Text, Divider } from "@chakra-ui/react";
 import {
   fetchPersonInfo,
   selectPerson,
   selectPersonStatus,
-} from "../../app/features/actors/personSlice.js";
-import { Box, Text, Spinner, VStack } from "@chakra-ui/react";
+} from "../../app/features/actors/personSlice";
 import {
   fetchPersonMovieCredits,
   selectPersonMovieCredits,
-} from "../../app/features/actors/personMoviesSlice.js";
-import { useLocation } from "react-router-dom";
-import PersonCredits from "./PersonCredits.js";
+} from "../../app/features/actors/personMoviesSlice";
+import { useParams } from "react-router-dom";
+import PersonCredits from "./PersonCredits";
 import {
   fetchPersonExternalIds,
   selectPersonExternalIds,
-} from "../../app/features/actors/personExternalIdsSlice.js";
-import ActorHeader from "./ActorHeader.js";
-import Biography from "./Biography.js";
-import SocialIdentityLinks from "./SocialIdentityLinks.js";
+} from "../../app/features/actors/personExternalIdsSlice";
+import ActorHeader from "./ActorHeader";
+import Biography from "./Biography";
+import SocialIdentityLinks from "./SocialIdentityLinks";
 
 const ActorDetails = () => {
   const dispatch = useDispatch();
+  const { id } = useParams();
   const person = useSelector(selectPerson);
   const status = useSelector(selectPersonStatus);
   const movieCredits = useSelector(selectPersonMovieCredits);
   const externalIds = useSelector(selectPersonExternalIds);
-  const location = useLocation();
-  const actorId = location.state.id;
 
   useEffect(() => {
-    dispatch(fetchPersonInfo(actorId));
-    dispatch(fetchPersonMovieCredits(actorId));
-    dispatch(fetchPersonExternalIds(actorId));
-  }, [dispatch, actorId]);
+    if (id) {
+      dispatch(fetchPersonInfo(id));
+      dispatch(fetchPersonMovieCredits(id));
+      dispatch(fetchPersonExternalIds(id));
+    }
+  }, [dispatch, id]);
 
-  if (status === "loading") {
-    return (
-      <VStack spacing={4} align="center">
-        <Spinner size="xl" color="teal.500" />
-        <Text>Loading...</Text>
-      </VStack>
-    );
-  }
+  const renderContent = () => {
+    if (status === "loading") {
+      return (
+        <VStack justify="center" align="center" spacing={4}>
+          <Spinner size="xl" color="teal.500" />
+          <Text>Loading...</Text>
+        </VStack>
+      );
+    }
 
-  if (status === "failed") {
+    if (status === "failed") {
+      return (
+        <Box justify="center" align="center">
+          <Text fontSize="xl" color="red.500">
+            Failed to load actor details.
+          </Text>
+        </Box>
+      );
+    }
+
+    if (!person) {
+      return null;
+    }
+
     return (
-      <Box>
-        <Text fontSize="xl" color="red.500">
-          Failed to load actor details.
-        </Text>
+      <Box
+        p={["2", "2", "3", "3", "4"]}
+        m={["1", "1", "2", "2", "3"]}
+        borderWidth={["1px", "1px", "2px", "2px", "3px"]}
+        borderRadius={["md", "md", "lg", "lg", "xl"]}
+        boxShadow={["sm", "sm", "md", "md", "lg"]}
+        borderColor="teal.400"
+        bgColor="white">
+        <ActorHeader person={person} movieCreditsLength={movieCredits.length} />
+        <Divider />
+        <Biography biography={person.biography} />
+        <Divider />
+        <SocialIdentityLinks externalIds={externalIds} />
+        <Divider />
+        <PersonCredits movieCredits={movieCredits} />
       </Box>
     );
-  }
+  };
 
-  if (!person) {
-    return null;
-  }
-
-  return (
-    <Box
-      p={["2", "2", "3", "3", "4"]}
-      m={["1", "1", "2", "2", "3"]}
-      borderWidth={["1px", "1px", "2px", "2px", "3px"]}
-      borderRadius={["md", "md", "lg", "lg", "xl"]}
-      boxShadow={["sm", "sm", "md", "md", "lg"]}
-      borderColor="teal.400"
-      bgColor="white">
-      <ActorHeader person={person} movieCreditsLength={movieCredits.length} />
-      <Biography biography={person.biography} />
-      <SocialIdentityLinks externalIds={externalIds} />
-      <PersonCredits movieCredits={movieCredits} />
-    </Box>
-  );
+  return renderContent();
 };
 
 export default ActorDetails;
