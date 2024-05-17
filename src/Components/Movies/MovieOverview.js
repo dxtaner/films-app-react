1acd69278c4a5340a4fee7b6c4af541b9c7a0c03
taner <tanerozer16@gmail.com>
2024-05-17 17:08:00 +0300

    PaginationButton added

--- src/Components/Movies/MovieOverview.js
+++ src/Components/Movies/MovieOverview.js
@@ -19,43 +19,37 @@ const MovieOverview = ({ movieDetails }) => {
   } = movieDetails;
 
   const formattedProductionCountries = production_countries
-    .map((country) => country.name)
-    .join(", ");
+    ? production_countries.map((country) => country.name).join(", ")
+    : "";
 
   const formattedSpokenLanguages = spoken_languages
-    .map((language) => language.english_name)
-    .join(", ");
+    ? spoken_languages.map((language) => language.english_name).join(", ")
+    : "";
 
-  // Dakikayı saate çevirme
-  const hours = Math.floor(runtime / 60);
-  const minutes = runtime % 60;
-  const formattedRuntime = `${hours} saat ${minutes} dakika`;
+  const formattedRuntime = runtime
+    ? `${Math.floor(runtime / 60)} saat ${runtime % 60} dakika`
+    : "";
 
   return (
     <Box
       p={6}
-      boxShadow="md"
       borderRadius="lg"
-      bg="white"
-      maxW="800px"
+      bg="gray.100"
       mx="auto"
       borderWidth={2}
       borderColor="gray.300"
       textAlign="left">
-      <Flex align="center" justify="space-between" mb={4}>
-        <Box>
-          <Text fontSize="3xl" fontWeight="bold" textAlign="center">
-            {original_title}
-          </Text>
-          <Badge colorScheme="teal" mt={2}>
-            Popülerlik: {popularity.toFixed(2)}
-          </Badge>
-        </Box>
-        <Box>
-          <Text fontSize="lg" fontWeight="bold">
-            Vizyon Tarihi: {release_date}
-          </Text>
-        </Box>
+      <Text fontSize="3xl" fontWeight="bold" textAlign="center" mb={4}>
+        {original_title}
+      </Text>
+
+      <Flex justify="space-between" align="center" flexWrap="wrap" mb={4}>
+        <Badge colorScheme="teal" mb={2}>
+          Popülerlik: {popularity ? popularity.toFixed(2) : ""}
+        </Badge>
+        <Text fontWeight="bold" mb={2}>
+          Vizyon Tarihi: {release_date ? release_date : ""}
+        </Text>
       </Flex>
 
       <Text fontSize="lg" mb={4}>
@@ -65,25 +59,33 @@ const MovieOverview = ({ movieDetails }) => {
       <Divider my={4} />
 
       <VStack spacing={4} align="left">
-        <Flex justify="space-between" w="100%">
+        <Flex justify="space-between" p={1} w="100%" mb={2} wrap={"wrap"}>
           <Text fontWeight="bold">Süre:</Text>
           <Text>{formattedRuntime}</Text>
         </Flex>
-        <Flex justify="space-between" w="100%">
+
+        <Flex justify="space-between" p={1} w="100%" mb={2} wrap={"wrap"}>
           <Text fontWeight="bold">Oy Ortalaması:</Text>
-          <Text>{vote_average.toFixed(2)}</Text>
+          <Text>{vote_average ? vote_average.toFixed(2) : ""}</Text>
         </Flex>
-        <Flex justify="space-between" w="100%">
+
+        <Flex justify="space-between" p={1} w="100%" mb={2} wrap={"wrap"}>
           <Text fontWeight="bold">Oy Sayısı:</Text>
-          <Text>{vote_count}</Text>
+          <Text>{vote_count ? vote_count : ""}</Text>
         </Flex>
-        <Flex justify="space-between" w="100%">
+
+        <Flex justify="space-between" p={1} w="100%" mb={2} wrap={"wrap"}>
           <Text fontWeight="bold">Üretim Ülkeleri:</Text>
-          <Badge p={3}>{formattedProductionCountries}</Badge>
+          <Badge colorScheme="red" m={1} p={2}>
+            {formattedProductionCountries}
+          </Badge>
         </Flex>
-        <Flex justify="space-between" w="100%">
+
+        <Flex justify="space-between" p={1} w="100%" mb={2} wrap={"wrap"}>
           <Text fontWeight="bold">Konuşulan Diller:</Text>
-          <Badge p={3}>{formattedSpokenLanguages}</Badge>
+          <Badge colorScheme="red" m={1} p={2}>
+            {formattedSpokenLanguages}
+          </Badge>
         </Flex>
       </VStack>
     </Box>
