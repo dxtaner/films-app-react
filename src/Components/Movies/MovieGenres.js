93062178e48d327b073455fcf9b4a10a7a2a3688
taner <tanerozer16@gmail.com>
2024-05-17 17:07:46 +0300

    MovieGenres edited

--- src/Components/Movies/MovieGenres.js
+++ src/Components/Movies/MovieGenres.js
@@ -2,20 +2,42 @@ import React from "react";
 import { HStack, Tag } from "@chakra-ui/react";
 
 const MovieGenres = ({ genres }) => {
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "teal",
+    "blue",
+    "cyan",
+    "purple",
+    "pink",
+    "gray",
+  ];
+
+  let colorIndex = 0;
+
   return (
-    <HStack spacing={2} mt={4} mb={2} justifyContent="center">
-      {genres?.map((gen, index) => (
-        <Tag
-          key={index}
-          colorScheme="red"
-          bg="gray.300"
-          borderRadius="lg"
-          p={2}
-          fontSize="sm"
-          fontWeight="bold">
-          {gen.name}
-        </Tag>
-      ))}
+    <HStack wrap={"wrap"} spacing={2} mt={4} mb={2} justifyContent="center">
+      {genres?.map((genre, index) => {
+        const color = colors[colorIndex];
+        colorIndex = (colorIndex + 1) % colors.length;
+
+        return (
+          <Tag
+            key={index}
+            colorScheme={color}
+            variant="solid"
+            borderRadius="full"
+            px={3}
+            py={2}
+            fontSize="md"
+            fontWeight="900"
+            _hover={{ bg: `${color}.700` }}>
+            {genre.name}
+          </Tag>
+        );
+      })}
     </HStack>
   );
 };
