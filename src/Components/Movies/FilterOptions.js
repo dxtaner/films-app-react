4fedf51f12470f96792ba136c69bea0b45a648cd
taner <tanerozer16@gmail.com>
2024-05-20 09:59:14 +0300

    FilterOptions edited

--- src/Components/Movies/FilterOptions.js
+++ src/Components/Movies/FilterOptions.js
@@ -1,220 +1,180 @@
 import React from "react";
-import { Box, Text, Grid, GridItem } from "@chakra-ui/react";
-import OriginCountryFilter from "./FilteredMovies/OriginCountryFilter";
-import OriginalLanguageFilter from "./FilteredMovies/OriginalLanguageFilter";
-import YearFilter from "./FilteredMovies/YearFilter";
-import CastFilter from "./FilteredMovies/CastFilter";
-import MinRuntimeFilter from "./FilteredMovies/MinRuntimeFilter";
-import MaxRuntimeFilter from "./FilteredMovies/MaxRuntimeFilter";
-import MinVoteCountFilter from "./FilteredMovies/MinVoteCountFilter";
-import MaxVoteCountFilter from "./FilteredMovies/MaxVoteCountFilter";
-import MinRatingSlider from "./FilteredMovies/MinRatingSlider";
-import MaxRatingSlider from "./FilteredMovies/MaxRatingSlider";
-import RegionFilter from "./FilteredMovies/RegionFilter";
-import PrimaryReleaseYearFilter from "./FilteredMovies/PrimaryReleaseYearFilter";
-import EndDateFilter from "./FilteredMovies/EndDateFilter";
-import StartDateFilter from "./FilteredMovies/StartDateFilter";
+import { Grid, Box, Text, Divider, Stack } from "@chakra-ui/react";
 import SortByFilter from "./FilteredMovies/SortByFilter";
 import CertificationFilter from "./FilteredMovies/CertificationFilter";
 import IncludeAdultFilter from "./FilteredMovies/IncludeAdultFilter";
 import IncludeVideoFilter from "./FilteredMovies/IncludeVideoFilter";
+import StartDateFilter from "./FilteredMovies/StartDateFilter";
+import EndDateFilter from "./FilteredMovies/EndDateFilter";
+import PrimaryReleaseYearFilter from "./FilteredMovies/PrimaryReleaseYearFilter";
 import GenreFilter from "./FilteredMovies/GenreFilter";
 import WithoutFilterGenreFilter from "./FilteredMovies/WithoutGenreFilter";
+import OriginCountryFilter from "./FilteredMovies/OriginCountryFilter";
+import OriginalLanguageFilter from "./FilteredMovies/OriginalLanguageFilter";
+import RuntimeFilter from "./FilteredMovies/RuntimeFilter";
+import VoteCountRangeFilter from "./FilteredMovies/VoteCountRangeFilter";
+import RatingRangeFilter from "./FilteredMovies/RatingRangeFilter";
 
 const FilterOptions = ({ queryParams, onFilterChange }) => {
   return (
-    <Box>
-      <Text fontSize="md" fontWeight="bold" mb={10}>
-        Filtre Seçenekleri
-      </Text>
-      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
+    <Box mt={4}>
+      <Grid
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+        }}
+        gap={6}>
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
             Sırala
           </Text>
+          <Divider my={2} borderColor="teal.500" />
+
           <SortByFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+        </Box>
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
             Sertifika
           </Text>
+          <Divider my={2} borderColor="teal.500" />
+
           <CertificationFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+        </Box>
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
             Yetişkin İçerik
           </Text>
+          <Divider my={2} borderColor="teal.500" />
+
           <IncludeAdultFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+        </Box>
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
             Video İçerik
           </Text>
+          <Divider my={2} borderColor="teal.500" />
+
           <IncludeVideoFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+        </Box>
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Türler:
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
+            Tarih Filtreleri
           </Text>
+          <Divider my={2} borderColor="teal.500" />
+
+          <Stack spacing={2}>
+            <StartDateFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
+            <EndDateFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
+            <PrimaryReleaseYearFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
+            Oylama Filtreleri
+          </Text>
+          <Divider my={2} borderColor="teal.500" />
+
+          <Stack spacing={2}>
+            <RatingRangeFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
+
+            <VoteCountRangeFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
+            Tür Filtreleri
+          </Text>
+          <Divider my={2} borderColor="teal.500" />
+
           <GenreFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Türü Hariç Tut
-          </Text>
           <WithoutFilterGenreFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+        </Box>
 
-        <GridItem>
-          <StartDateFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <EndDateFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Yayın Yılı
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
+            Film Ülke Filtreleri
           </Text>
-          <PrimaryReleaseYearFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+          <Stack spacing={2}>
+            <OriginCountryFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Bölge
+            <OriginalLanguageFilter
+              queryParams={queryParams}
+              onFilterChange={onFilterChange}
+            />
+            <Divider my={2} borderColor="teal.500" />
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontSize="lg" fontWeight="bold">
+            Film Süresi Filtreleri
           </Text>
-          <RegionFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
 
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Oylama Ort. (Min)
-          </Text>
-          <MinRatingSlider
+          <RuntimeFilter
             queryParams={queryParams}
             onFilterChange={onFilterChange}
           />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Oylama Ort. (Max)
-          </Text>
-          <MaxRatingSlider
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <MinVoteCountFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <MaxVoteCountFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Min DK
-          </Text>
-          <MinRuntimeFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Max DK
-          </Text>
-          <MaxRuntimeFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Oyuncular
-          </Text>
-          <CastFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Yıl
-          </Text>
-          <YearFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Ülke
-          </Text>
-          <OriginCountryFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
-
-        <GridItem>
-          <Text fontSize="sm" fontWeight="semibold">
-            Dil
-          </Text>
-          <OriginalLanguageFilter
-            queryParams={queryParams}
-            onFilterChange={onFilterChange}
-          />
-        </GridItem>
+          <Divider my={2} borderColor="teal.500" />
+        </Box>
       </Grid>
     </Box>
   );
