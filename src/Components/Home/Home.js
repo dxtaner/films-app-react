0d1fc5fec949a4bb97c7db34b440c531cf7dbce1
taner <tanerozer16@gmail.com>
2024-05-20 09:57:44 +0300

    Home edited

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Box, VStack, StackDivider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import PopularMoviesCarousel from "../Movies/popularMoviesCarousel.js";
 import TopRatedMovies from "../Movies/TopRatedMovies.js";
-import Title from "../Title/titles.js";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -18,16 +17,7 @@ const Home = () => {
         <PopularMoviesCarousel />
       </Box>
 
-      <VStack
-        spacing={5}
-        p={4}
-        divider={<StackDivider borderColor="blue.800" />}
-        alignItems="stretch">
-        <Title text="En Çok Puan Alan Filmler">
-          Şimdiki zamana kadarki en değerli filmler
-        </Title>
-        <TopRatedMovies handleDetails={showDetails} />
-      </VStack>
+      <TopRatedMovies handleDetails={showDetails} />
     </Box>
   );
 };
