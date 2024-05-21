8733a8d0a0bcf73ee39f26dd58353707c3352226
taner <tanerozer16@gmail.com>
2024-05-21 18:24:47 +0300

    CertificationFilter edited

--- src/Components/Movies/FilteredMovies/CertificationFilter.js
+++ src/Components/Movies/FilteredMovies/CertificationFilter.js
@@ -1,23 +1,37 @@
 import React from "react";
-import { Select } from "@chakra-ui/react";
-import renderSelectOptions from "./renderSelectOptions";
+import { Select, FormControl, FormLabel, Box, Divider } from "@chakra-ui/react";
 
 const CertificationFilter = ({ queryParams, onFilterChange }) => {
   return (
-    <Select
-      value={queryParams.certification}
-      onChange={(e) => onFilterChange("certification", e.target.value)}
-      size="xs"
-      borderWidth="1px"
-      borderRadius="sm"
-      borderColor="gray.300"
-      _focus={{ borderColor: "blue.400", boxShadow: "none" }}
-      maxWidth="150px">
-      {renderSelectOptions([
-        { id: "PG-13", name: "PG-13" },
-        { id: "R", name: "R" },
-      ])}
-    </Select>
+    <Box
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      boxShadow="md"
+      bg="white"
+      borderColor="gray.200"
+      _hover={{ boxShadow: "lg", borderColor: "gray.300" }}>
+      <FormControl>
+        <FormLabel fontSize="md" fontWeight="bold" mb={2} color="teal.600">
+          Sertifika
+        </FormLabel>
+        <Divider my={2} borderColor="teal.500" />
+
+        <Select
+          value={queryParams.certification}
+          onChange={(e) => onFilterChange("certification", e.target.value)}
+          size="md"
+          borderRadius="md"
+          borderColor="gray.300"
+          _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
+          _hover={{ borderColor: "gray.400" }}
+          backgroundColor="white"
+          color="gray.800">
+          <option value="PG-13">PG-13</option>
+          <option value="R">R</option>
+        </Select>
+      </FormControl>
+    </Box>
   );
 };
 
