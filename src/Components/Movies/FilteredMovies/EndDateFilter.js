d398081e0d056fc98460064a9a43c7c3e6ff2e28
taner <tanerozer16@gmail.com>
2024-05-21 18:23:41 +0300

    EndDateFilter edited

--- src/Components/Movies/FilteredMovies/EndDateFilter.js
+++ src/Components/Movies/FilteredMovies/EndDateFilter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Input, Text } from "@chakra-ui/react";
+import { Input, FormControl, FormLabel, Box, Divider } from "@chakra-ui/react";
 
 const EndDateFilter = ({ queryParams, onFilterChange }) => {
   const handleEndDateChange = (e) => {
@@ -7,22 +7,34 @@ const EndDateFilter = ({ queryParams, onFilterChange }) => {
   };
 
   return (
-    <div>
-      <Text fontSize="sm" fontWeight="semibold">
-        Bitiş Tarihi
-      </Text>
-      <Input
-        type="date"
-        value={queryParams.primary_release_date.lte || ""}
-        onChange={handleEndDateChange}
-        size="xs"
-        borderWidth="1px"
-        borderRadius="sm"
-        borderColor="gray.300"
-        _focus={{ borderColor: "blue.400", boxShadow: "none" }}
-        maxWidth="150px"
-      />
-    </div>
+    <Box
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      boxShadow="md"
+      bg="white"
+      borderColor="gray.200"
+      _hover={{ boxShadow: "lg", borderColor: "gray.300" }}>
+      <FormControl>
+        <FormLabel fontSize="md" fontWeight="semibold" mb={1} color="teal.600">
+          Bitiş Tarihi
+        </FormLabel>
+        <Divider my={2} borderColor="teal.500" />
+
+        <Input
+          type="date"
+          value={queryParams["primary_release_date.lte"] || ""}
+          onChange={handleEndDateChange}
+          size="sm"
+          borderRadius="md"
+          borderColor="gray.300"
+          _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
+          _hover={{ borderColor: "gray.400" }}
+          backgroundColor="white"
+          color="gray.800"
+        />
+      </FormControl>
+    </Box>
   );
 };
 
