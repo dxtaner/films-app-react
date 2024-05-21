04b30eb7abde89779c5af2424f035cc12c133c7e
taner <tanerozer16@gmail.com>
2024-05-21 18:25:39 +0300

    IncludeVideoFilter edited

--- src/Components/Movies/FilteredMovies/IncludeVideoFilter.js
+++ src/Components/Movies/FilteredMovies/IncludeVideoFilter.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { Select } from "@chakra-ui/react";
-import renderSelectOptions from "./renderSelectOptions";
+import { Select, FormControl, FormLabel, Box, Divider } from "@chakra-ui/react";
 
 const IncludeVideoFilter = ({ queryParams, onFilterChange }) => {
   return (
-    <Select
-      value={queryParams.include_video.toString()}
-      onChange={(e) =>
-        onFilterChange("include_video", e.target.value === "true")
-      }
-      size="xs"
-      borderWidth="1px"
-      borderRadius="sm"
-      borderColor="gray.300"
-      _focus={{ borderColor: "blue.400", boxShadow: "none" }}
-      maxWidth="150px">
-      {renderSelectOptions([
-        { id: "true", name: "Evet" },
-        { id: "false", name: "Hayır" },
-      ])}
-    </Select>
+    <Box
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      boxShadow="md"
+      bg="white"
+      borderColor="gray.200"
+      _hover={{ boxShadow: "lg", borderColor: "gray.300" }}>
+      <FormControl>
+        <FormLabel fontSize="md" fontWeight="bold" mb={2} color="teal.600">
+          Video İçeriği
+        </FormLabel>
+        <Divider my={2} borderColor="teal.500" />
+
+        <Select
+          value={queryParams.include_video.toString()}
+          onChange={(e) =>
+            onFilterChange("include_video", e.target.value === "true")
+          }
+          size="md"
+          borderRadius="md"
+          borderColor="gray.300"
+          _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
+          _hover={{ borderColor: "gray.400" }}
+          backgroundColor="white"
+          color="gray.800">
+          <option value="true">Evet</option>
+          <option value="false">Hayır</option>
+        </Select>
+      </FormControl>
+    </Box>
   );
 };
 
