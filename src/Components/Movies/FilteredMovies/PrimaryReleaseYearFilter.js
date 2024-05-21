cebcd4a7c9d13014f22a093ce4e30190c20092f2
taner <tanerozer16@gmail.com>
2024-05-21 18:26:11 +0300

    SortByFilter edited

--- src/Components/Movies/FilteredMovies/PrimaryReleaseYearFilter.js
+++ src/Components/Movies/FilteredMovies/PrimaryReleaseYearFilter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
+import { Input, FormControl, FormLabel, Box, Divider } from "@chakra-ui/react";
 
 const PrimaryReleaseYearFilter = ({ queryParams, onFilterChange }) => {
   const handleYearChange = (e) => {
@@ -8,18 +8,37 @@ const PrimaryReleaseYearFilter = ({ queryParams, onFilterChange }) => {
   };
 
   return (
-    <Input
-      type="number"
-      value={queryParams.primary_release_year || ""}
-      onChange={handleYearChange}
-      size="xs" // Küçük boyutta giriş alanı
-      borderWidth="1px" // İnce kenarlık
-      borderRadius="sm" // Hafif yuvarlak kenarlar
-      borderColor="gray.300" // Kenarlık rengi
-      _focus={{ borderColor: "blue.400", boxShadow: "none" }} // Odaklandığında stillendirme
-      maxWidth="150px" // Maksimum genişlik
-      placeholder="Yıl"
-    />
+    <Box
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      boxShadow="md"
+      bg="white"
+      borderColor="gray.200"
+      _hover={{ boxShadow: "lg", borderColor: "gray.300" }}>
+      <FormControl>
+        <FormLabel fontSize="md" fontWeight="semibold" mb={1} color="teal.600">
+          Yıl
+        </FormLabel>
+        <Divider my={2} borderColor="teal.500" />
+
+        <Input
+          type="number"
+          value={queryParams.primary_release_year || ""}
+          onChange={handleYearChange}
+          size="sm"
+          borderRadius="md"
+          borderColor="gray.300"
+          _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
+          _hover={{ borderColor: "gray.400" }}
+          backgroundColor="white"
+          color="gray.800"
+          placeholder="Yıl"
+          min={1900}
+          max={new Date().getFullYear()}
+        />
+      </FormControl>
+    </Box>
   );
 };
 
