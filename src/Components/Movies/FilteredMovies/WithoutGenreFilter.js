9d805b066bc9ed045de14c46132124c1ec534132
taner <tanerozer16@gmail.com>
2024-05-22 15:46:37 +0300

    WithoutGenreFilter edited

--- src/Components/Movies/FilteredMovies/WithoutGenreFilter.js
+++ src/Components/Movies/FilteredMovies/WithoutGenreFilter.js
@@ -1,26 +1,40 @@
 import React from "react";
-import { Select } from "@chakra-ui/react";
+import { Select, Box, Text, Divider } from "@chakra-ui/react";
 import genreOptions from "../genreOptions";
 
 const WithoutGenreFilter = ({ queryParams, onFilterChange }) => {
   return (
-    <Select
-      value={queryParams.without_genres || ""}
-      onChange={(e) => onFilterChange("without_genres", e.target.value)}
-      size="xs" // Ekstra küçük boyutta seçim kutusu
-      borderWidth="1px" // İnce kenarlık
-      borderRadius="sm" // Hafif yuvarlak kenarlar
-      borderColor="gray.300" // Kenarlık rengi
-      _focus={{ borderColor: "blue.400", boxShadow: "none" }} // Odaklandığında stillendirme
-      maxWidth="150px" // Maksimum genişlik
-    >
-      <option value="">Seçmeme</option>
-      {genreOptions.map((genre) => (
-        <option key={genre.id} value={genre.id}>
-          {genre.name}
-        </option>
-      ))}
-    </Select>
+    <Box
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      boxShadow="md"
+      bg="white"
+      borderColor="gray.200"
+      _hover={{ boxShadow: "lg", borderColor: "gray.300" }}>
+      <Text fontSize="md" fontWeight="bold" color="teal.600">
+        Türleri Kaldır
+      </Text>
+      <Divider my={2} borderColor="teal.500" />
+      <Select
+        value={queryParams.without_genres || ""}
+        onChange={(e) => onFilterChange("without_genres", e.target.value)}
+        size="md"
+        borderWidth="1px"
+        borderRadius="md"
+        borderColor="gray.300"
+        _focus={{ borderColor: "teal.400", boxShadow: "0 0 0 1px teal.400" }}
+        _hover={{ borderColor: "gray.400" }}
+        backgroundColor="white"
+        color="gray.800"
+        placeholder="Seçmeme">
+        {genreOptions.map((genre) => (
+          <option key={genre.id} value={genre.id}>
+            {genre.name}
+          </option>
+        ))}
+      </Select>
+    </Box>
   );
 };
 
