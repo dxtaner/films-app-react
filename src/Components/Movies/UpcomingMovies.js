70a4dbbad2602f5928fa4f3f0ef2483701a9d821
taner <tanerozer16@gmail.com>
2024-05-23 15:29:10 +0300

    UpcomingMovies edited

--- src/Components/Movies/UpcomingMovies.js
+++ src/Components/Movies/UpcomingMovies.js
@@ -8,23 +8,40 @@ import {
   Box,
   Center,
   Spinner,
+  Button,
 } from "@chakra-ui/react";
 import MovieCard from "../Cards/MovieCards";
+import Title from "../Title/titles";
 import {
   getUpcoming,
   upcomingList,
   upcomingLoading,
+  currentPage,
+  totalPages,
+  setCurrentPage,
 } from "../../app/features/movies/upcomingSlice";
-import Title from "../Title/titles";
+import { useLocation } from "react-router-dom";
 
 const UpcomingMovies = () => {
   const dispatch = useDispatch();
+  const location = useLocation();
+
   const upcomingMovies = useSelector(upcomingList);
   const isLoading = useSelector(upcomingLoading);
+  const currentPageNumber = useSelector(currentPage);
+  const totalPageCount = useSelector(totalPages);
 
   useEffect(() => {
-    dispatch(getUpcoming());
-  }, [dispatch]);
+    dispatch(setCurrentPage(1));
+  }, [dispatch, location.pathname]);
+
+  useEffect(() => {
+    dispatch(getUpcoming(currentPageNumber));
+  }, [dispatch, currentPageNumber]);
+
+  const handleLoadMore = () => {
+    dispatch(setCurrentPage(currentPageNumber + 1));
+  };
 
   return (
     <VStack
@@ -36,19 +53,31 @@ const UpcomingMovies = () => {
           <Text>Yaklaşan ve Vizyondaki tüm Filmlerin listesi</Text>
         </Title>
       </Box>
-      {isLoading ? (
+      {isLoading && currentPageNumber === 1 ? (
         <Center>
           <Spinner size="xl" />
         </Center>
       ) : (
-        <SimpleGrid
-          justifyItems="center"
-          columns={{ base: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
-          spacing={4}>
-          {upcomingMovies.map((item) => (
-            <MovieCard key={item.id} movie={item} />
-          ))}
-        </SimpleGrid>
+        <>
+          <SimpleGrid
+            justifyItems="center"
+            columns={{ base: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
+            spacing={4}>
+            {upcomingMovies.map((item) => (
+              <MovieCard key={item.id} movie={item} />
+            ))}
+          </SimpleGrid>
+          {currentPageNumber < totalPageCount && (
+            <Box mt={4} w="100%" display="flex" justifyContent="center">
+              <Button
+                onClick={handleLoadMore}
+                colorScheme="blue"
+                disabled={isLoading}>
+                Daha Fazla Yükle
+              </Button>
+            </Box>
+          )}
+        </>
       )}
     </VStack>
   );
