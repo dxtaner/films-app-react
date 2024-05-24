1a79c42f22f4dc37a50ff9dc110fd809c2bae744
taner <tanerozer16@gmail.com>
2024-05-24 16:16:27 +0300

    SearchBar edited

--- src/Components/Header/Navbar/SearchBar.js
+++ src/Components/Header/Navbar/SearchBar.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Input, Box } from "@chakra-ui/react";
+import {
+  Input,
+  Box,
+  IconButton,
+  InputGroup,
+  InputRightElement,
+  Flex,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import { searchMoviesAsync } from "../../../app/features/movies/searchSlice.js";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -47,32 +55,45 @@ const SearchBar = () => {
   };
 
   return (
-    <Box display="flex" justifyContent="center" margin="20px">
+    <Flex justifyContent="center" mt={6} mb={6} width="full">
       <Box
         position="relative"
-        width="1000px"
+        width={{ base: "full", md: "800px" }}
         bg="white"
         borderRadius="xl"
         boxShadow="md"
-        p="4">
-        <Input
-          placeholder="Film Arayın.."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyPress={handleKeyPress}
-          borderRadius="xl"
-          borderColor="blue.300"
-          _hover={{ borderColor: "blue.500" }}
-          _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
-        />
-        {query && (
-          <SearchResults
-            results={searchResults}
-            handleResultClick={handleResultClick}
+        p={4}>
+        <InputGroup>
+          <Input
+            placeholder="Film Arayın.."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={handleKeyPress}
+            borderRadius="xl"
+            borderColor="blue.300"
+            _hover={{ borderColor: "blue.500" }}
+            _focus={{ borderColor: "blue.500", boxShadow: "outline" }}
           />
+          <InputRightElement>
+            <IconButton
+              aria-label="Search Movies"
+              icon={<SearchIcon />}
+              onClick={handleSearch}
+              colorScheme="blue"
+              borderRadius="xl"
+            />
+          </InputRightElement>
+        </InputGroup>
+        {query && (
+          <Box position="absolute" top="60px" width="full" zIndex={10}>
+            <SearchResults
+              results={searchResults}
+              handleResultClick={handleResultClick}
+            />
+          </Box>
         )}
       </Box>
-    </Box>
+    </Flex>
   );
 };
 
