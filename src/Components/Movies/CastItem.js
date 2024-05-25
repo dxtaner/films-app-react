e7db4d14e0a02879a353bb63f784973562934939
taner <tanerozer16@gmail.com>
2024-05-25 18:18:24 +0300

    CastItem added

---
+++ src/Components/Movies/CastItem.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Box,
+  Image,
+  VStack,
+  Heading,
+  Text,
+  Badge,
+  Tooltip,
+} from "@chakra-ui/react";
+
+const CastItem = ({ credit, showDetails }) => {
+  return (
+    <Tooltip label="Detayları görmek için tıklayın" fontSize="md">
+      <Box
+        textAlign="center"
+        boxShadow="md"
+        borderRadius="lg"
+        bg="gray.50"
+        p={4}
+        cursor="pointer"
+        onClick={() => showDetails(credit)}
+        transition="transform 0.3s ease-in-out"
+        _hover={{ transform: "scale(1.03)" }}>
+        <Image
+          src={
+            credit.profile_path
+              ? `https://image.tmdb.org/t/p/original${credit.profile_path}`
+              : "https://via.placeholder.com/150"
+          }
+          alt={credit.name}
+          borderRadius="full"
+          boxSize="180px"
+          mx="auto"
+        />
+        <VStack spacing={2} mt={4}>
+          <Heading
+            fontSize={["sm", "md", "lg", "xl", "2xl"]}
+            fontWeight="semibold">
+            {credit.name}
+          </Heading>
+          {credit.character && (
+            <Text
+              fontSize={["xs", "sm", "md", "lg", "lg"]}
+              fontWeight="semibold"
+              color="gray.600">
+              Karakter: {credit.character}
+            </Text>
+          )}
+          <Text
+            fontSize={["xs", "sm", "md", "lg", "lg"]}
+            fontWeight="semibold"
+            color="gray.600">
+            Cinsiyet: {credit.gender === 1 ? "Kadın" : "Erkek"}
+          </Text>
+          <Badge
+            colorScheme="teal"
+            variant="solid"
+            fontSize={["xs", "sm", "sm", "md", "md"]}>
+            Popülerlik: {credit.popularity.toFixed(2)}
+          </Badge>
+        </VStack>
+      </Box>
+    </Tooltip>
+  );
+};
+
+export default CastItem;
