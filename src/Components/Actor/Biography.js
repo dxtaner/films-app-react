09981bce24825fa30b8f45ea9dac2c6b8d1e2156
taner <tanerozer16@gmail.com>
2024-05-28 17:15:01 +0300

    Biography edited

--- src/Components/Actor/Biography.js
+++ src/Components/Actor/Biography.js
@@ -1,19 +1,62 @@
-import React from "react";
-import { Text, VStack, Heading, Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Text, VStack, Heading, Box, Link, Flex } from "@chakra-ui/react";
+import SocialIdentityLinks from "./SocialIdentityLinks";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchPersonExternalIds,
+  selectPersonExternalIds,
+} from "../../app/features/actors/personExternalIdsSlice";
+import { useParams } from "react-router-dom";
 
 function Biography({ biography }) {
+  const [expanded, setExpanded] = useState(false);
+  const previewLength = 255;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const shortenBiography = (text) => {
+    return text.length > previewLength
+      ? text.slice(0, previewLength) + "..."
+      : text;
+  };
+
+  const externalIds = useSelector(selectPersonExternalIds);
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchPersonExternalIds(id));
+    }
+  }, [dispatch, id]);
+
+  const handleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <Box p={4} borderRadius="lg" bg="gray.100">
-      <VStack spacing={4} align="start">
-        <Heading as="h2" size="lg" mb={2}>
+    <Box p={6} borderRadius="lg" bg="white" boxShadow="md">
+      <Flex
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+        direction={{ base: "column", md: "row" }}
+        mb={4}>
+        <Heading as="h2" size="lg" mb={{ base: 2, md: 0 }}>
           Biyografi
         </Heading>
+        <SocialIdentityLinks externalIds={externalIds} />
+      </Flex>
+      <VStack spacing={4} align="start">
         {biography ? (
-          <Text fontSize="lg" color="gray.600">
-            {biography}
-          </Text>
+          <>
+            <Text fontSize="lg" color="gray.700" overflowWrap="break-word">
+              {expanded ? biography : shortenBiography(biography)}
+            </Text>
+            {biography.length > previewLength && (
+              <Link color="blue.500" onClick={handleExpand} cursor="pointer">
+                {expanded ? "Daha az göster" : "Daha fazla göster"}
+              </Link>
+            )}
+          </>
         ) : (
-          <Text fontSize="lg" color="gray.600">
+          <Text fontSize="lg" color="gray.700">
             Biyografi mevcut değil.
           </Text>
         )}
