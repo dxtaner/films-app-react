2bd39473039f7c045aaedab34e1f429847c8a259
taner <tanerozer16@gmail.com>
2024-05-31 18:47:47 +0300

    person js edited

--- src/Components/Services/person.js
+++ src/Components/Services/person.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-// import { showErrorMessage } from "../Alerts.js";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const API_KEY = process.env.REACT_APP_APIKEY;
@@ -14,15 +13,9 @@ export const getPopularPersons = () => {
 export const getPersonInfo = (id) => {
   const requestURL = `${BASE_URL}/person/${id}?api_key=${API_KEY}&language=tr-US`;
 
-  // console.log("Person ID:", id); // Kişi kimliğini konsola yazdır
-  // console.log("Request URL:", requestURL); // İstek URL'sini konsola yazdır
-
   return axios
     .get(requestURL)
-    .then((response) => {
-      // console.log("Response Data:", response.data); // API yanıtını konsola yazdır
-      return response.data;
-    })
+    .then((response) => response.data)
     .catch((error) => {
       console.error(`Kişi bilgileri alınamadı (ID: ${id}): `, error);
       throw error;
@@ -34,9 +27,19 @@ export const getPersonMovieCredits = (personId) => {
     .get(
       `${BASE_URL}/person/${personId}/movie_credits?api_key=${API_KEY}&language=tr-US`
     )
-    .then((response) => {
-      return response.data;
-    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Kişi film kredileri alınamadı: ", error);
+      throw error;
+    });
+};
+
+export const fetchTVCredits = (personId) => {
+  return axios
+    .get(
+      `${BASE_URL}/person/${personId}/tv_credits?api_key=${API_KEY}&language=tr-US`
+    )
+    .then((response) => response.data)
     .catch((error) => {
       console.error("Kişi film kredileri alınamadı: ", error);
       throw error;
@@ -48,11 +51,37 @@ export const getPersonExternalIds = (personId) => {
     .get(
       `${BASE_URL}/person/${personId}/external_ids?api_key=${API_KEY}&language=tr-US`
     )
-    .then((response) => {
-      return response.data;
-    })
+    .then((response) => response.data)
     .catch((error) => {
       console.error("Kişi dış kimlikleri alınamadı: ", error);
       throw error;
     });
+};
+
+export const searchPersons = (query) => {
+  return axios
+    .get(
+      `${BASE_URL}/search/person?api_key=${API_KEY}&language=tr-US&query=${query}`
+    )
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Kişi araması yapılamadı: ", error);
+      throw error;
+    });
+};
+
+export const fetchPersonImages = async (personId) => {
+  const apiUrl = `${BASE_URL}/person/${personId}/images?api_key=${API_KEY}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Bir hata oluştu, lütfen tekrar deneyin.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Hata:", error.message);
+    return null;
+  }
 };
