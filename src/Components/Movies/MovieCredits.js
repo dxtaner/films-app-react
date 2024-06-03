c493720782c1f43224a110651e137a02b0f7d4a2
taner <tanerozer16@gmail.com>
2024-06-04 02:15:03 +0300

    MovieCredits edited

--- src/Components/Movies/MovieCredits.js
+++ src/Components/Movies/MovieCredits.js
@@ -1,112 +1,136 @@
 import React, { useEffect, useState } from "react";
+import { Box, Text, useColorModeValue, SimpleGrid } from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  Box,
-  Heading,
-  SimpleGrid,
-  Badge,
-  Text,
-  Button,
-  Image,
-  VStack,
-} from "@chakra-ui/react";
-import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
-import { useNavigate } from "react-router-dom";
+  creditList,
+  getCredit,
+} from "../../app/features/movies/details/creditSlice";
+import CastItem from "./CastItem";
+import CrewItem from "./CrewItem";
+import ToggleButton from "./ToggleButton";
+import PaginationButton from "./PaginationButton";
+import Title from "../Title/titles";
 
-const MovieCredits = ({ credits }) => {
-  const itemsPerPage = 4;
-  const [currentPage, setCurrentPage] = useState(1);
+const MovieCredits = () => {
+  const dispatch = useDispatch();
+  const movieCredits = useSelector(creditList);
+  const { id } = useParams();
   const navigate = useNavigate();
+
+  const itemsPerPage = 8;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isCastPage, setIsCastPage] = useState(true);
+
+  const bgColor = useColorModeValue("gray.50", "gray.800");
+  const textColor = useColorModeValue("gray.800", "gray.50");
+
+  useEffect(() => {
+    dispatch(getCredit(id));
+    setCurrentPage(1);
+  }, [dispatch, id]);
+
+  if (!movieCredits) {
+    return (
+      <Text m={2} align="center">
+        Yükleniyor...
+      </Text>
+    );
+  }
+  if (
+    !movieCredits ||
+    !movieCredits.cast ||
+    movieCredits.cast.length === 0 ||
+    !movieCredits.crew ||
+    movieCredits.crew.length === 0
+  ) {
+    return (
+      <Text m={2} align="center">
+        Oyuncu veya ekip üyesi bulunamadı.
+      </Text>
+    );
+  }
+
+  const showDetails = (person) => {
+    navigate(`/ActorDetails/${person.id}`, { state: person });
+  };
+
+  const handleTogglePage = () => {
+    setIsCastPage((prevIsCastPage) => !prevIsCastPage);
+    setCurrentPage(1);
+  };
+
+  const goToPreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const goToNextPage = () => {
+    const totalItems = isCastPage
+      ? movieCredits.cast.length
+      : movieCredits.crew.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const visibleCredits = credits.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(credits.length / itemsPerPage);
+  const visibleItems = isCastPage
+    ? movieCredits.cast.slice(startIndex, endIndex)
+    : movieCredits.crew.slice(startIndex, endIndex);
 
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-  };
-
-  const showDetails = (person) => {
-    navigate(`/actorDetails/${person.id}`, { state: person });
-  };
-
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [credits]);
+  const totalItems = isCastPage
+    ? movieCredits.cast.length
+    : movieCredits.crew.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   return (
     <Box
-      p={[1, 1, 3, 3]}
-      fontSize={["sm", "md", "lg", "xl"]}
+      p={[1, 2, 2, 4, 4]}
+      fontSize={["xs", "sm", "md", "lg", "xl"]}
       textAlign="center"
-      maxW="800px"
-      mx="auto">
-      <Heading fontSize="2xl" mb={4}>
-        Oyuncu Bilgileri
-      </Heading>
-      <SimpleGrid columns={[1, 2, 3, 4]} spacing={4} justifyItems="center">
-        {visibleCredits.map((credit, index) => (
-          <Box
-            key={index}
-            textAlign="center"
-            boxShadow="lg"
-            borderRadius="lg"
-            bg="gray.200"
-            p={4}
-            maxW="200px"
-            flexBasis="200px"
-            cursor="pointer"
-            onClick={() => showDetails(credit)}>
-            <Image
-              src={`https://image.tmdb.org/t/p/original${credit.profile_path}`}
-              alt={credit.name}
-              borderRadius="full"
-              boxSize="150px"
-              mx="auto"
+      mx="auto"
+      boxShadow="md"
+      bg={bgColor}
+      color={textColor}
+      borderRadius="lg">
+      <Title text={isCastPage ? "Oyuncu Kadrosu" : "Ekip Üyeleri"}></Title>
+      <SimpleGrid
+        columns={[1, 2, 3, 4]}
+        spacing={4}
+        m={2}
+        p={2}
+        justifyItems="center">
+        {visibleItems.map((credit) =>
+          isCastPage ? (
+            <CastItem
+              key={`cast-${credit.credit_id}`}
+              credit={credit}
+              showDetails={showDetails}
             />
-            <VStack spacing={4}>
-              <Heading fontSize="xl" mt={4}>
-                {credit.name}
-              </Heading>
-
-              <Text
-                fontSize="md"
-                mt={2}
-                fontWeight="semibold" // Metin kalınlaştırma
-                color="gray.700" // Metin rengi
-              >
-                Karakter: {credit.character}
-              </Text>
-
-              <Badge colorScheme="blue" fontSize="sm" mt={2}>
-                Popülerlik: {credit.popularity}
-              </Badge>
-            </VStack>
-          </Box>
-        ))}
+          ) : (
+            <CrewItem
+              key={`crew-${credit.credit_id}`}
+              credit={credit}
+              showDetails={showDetails}
+            />
+          )
+        )}
       </SimpleGrid>
-      <Box mt={4}>
-        <Button
-          colorScheme="pink"
-          onClick={() => handlePageChange(currentPage - 1)}
-          isDisabled={currentPage === 1}
-          leftIcon={<ArrowBackIcon />}
-          bg="pink.500"
-          _hover={{ bg: "pink.600" }}
-          _active={{ bg: "pink.700" }}>
-          Önceki Sayfadaki Oyuncular
-        </Button>
-        <Button
-          colorScheme="green"
-          onClick={() => handlePageChange(currentPage + 1)}
-          ml={2}
-          isDisabled={currentPage === totalPages}
-          rightIcon={<ArrowForwardIcon />}
-          bg="green.500"
-          _hover={{ bg: "green.600" }}
-          _active={{ bg: "green.700" }}>
-          Sonraki Sayfadaki Oyuncular
-        </Button>
+      <ToggleButton
+        isCastPage={isCastPage}
+        handleTogglePage={handleTogglePage}
+      />
+      <Box mt={4} textAlign="center">
+        <Text fontSize="sm" color="gray.500">
+          Toplam {isCastPage ? "Oyuncu Sayısı" : "Ekip Üyesi Sayısı"}:{" "}
+          {totalItems}, Şu Anki Sayfa: {currentPage} / {totalPages}
+        </Text>
+        <PaginationButton
+          currentPage={currentPage}
+          totalPages={totalPages}
+          goToPreviousPage={goToPreviousPage}
+          goToNextPage={goToNextPage}
+        />
       </Box>
     </Box>
   );
