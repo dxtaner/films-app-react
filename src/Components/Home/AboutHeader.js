e3995229eacb60c0dcbd529d992ae336fd73ddb8
Taner Özer <tanerozer16@gmail.com>
2026-09-23 15:09:22 +0300

    Refactor AboutHeader component layout and styling for improved user experience

--- src/Components/Home/AboutHeader.js
+++ src/Components/Home/AboutHeader.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Flex, Heading, Icon } from "@chakra-ui/react";
+import { FaFilm } from "react-icons/fa";
 
 function AboutHeader() {
   return (
-    <Heading
-      as="h2"
-      size="xl"
-      mb={4}
-      color="teal.900"
-      fontWeight="bold"
-      textAlign="center"
-      _hover={{ color: "teal.700", textDecoration: "underline" }}>
-      Hoş Geldiniz!
-    </Heading>
+    <Flex align="center" gap={3}>
+      <Icon as={FaFilm} boxSize={8} color="red.500" />
+      <Heading
+        as="h1"
+        size="2xl"
+        fontWeight="extrabold"
+        letterSpacing="tight"
+        color="gray.400"
+      >
+        Hoş Geldiniz!
+      </Heading>
+    </Flex>
   );
 }
 
