9d177537b960226e0891914b07fb3cc476fd2ce0
Taner Özer <tanerozer16@gmail.com>
2026-09-23 15:09:07 +0300

    Refactor About component layout and styling for improved user experience

--- src/Components/Home/About.js
+++ src/Components/Home/About.js
@@ -1,37 +1,65 @@
 import React from "react";
-import { Box, Center, Divider, Icon, Text, VStack } from "@chakra-ui/react";
-import { FaFilm } from "react-icons/fa";
+import { Box, Container, Stack, Flex, VStack } from "@chakra-ui/react";
 import MovieImage from "./MovieImage";
 import AboutHeader from "./AboutHeader";
 import AboutText from "./AboutText";
-import ExploreButton from "./ExploreButton";
 import MissionVision from "./MissionVision";
+import ExploreButton from "./ExploreButton";
 
 function About() {
   return (
-    <Box bgGradient="linear(to-r, teal.800, blue.100)" minH="100vh">
-      <Center py={8}>
-        <Box
-          p={8}
-          textAlign="center"
-          bgColor="white"
-          rounded="lg"
-          shadow="xl"
-          maxW="800px">
-          <Icon as={FaFilm} boxSize={16} color="blue.900" mb={4} />
-          <MovieImage />
-          <AboutHeader />
-          <AboutText />
-          <ExploreButton />
-          <Divider my={6} borderColor="gray.400" />
-          <VStack spacing={6} align="start">
-            <MissionVision />
-          </VStack>
-          <Text fontSize="sm" mt={6} color="gray.700">
-            © 2024 Film Dünyası
-          </Text>
-        </Box>
-      </Center>
+    <Box
+      bg="gray.950"
+      color="white"
+      minH="100vh"
+      position="relative"
+      py={{ base: 10, md: 20 }}
+      px={4}
+      overflow="hidden"
+    >
+      {/* Sinematik Kırmızı Vurgu Işığı */}
+      <Box
+        position="absolute"
+        top="-10%"
+        left="50%"
+        transform="translateX(-50%)"
+        w="600px"
+        h="400px"
+        bgGradient="radial(red.600 0%, transparent 70%)"
+        opacity={0.15}
+        filter="blur(60px)"
+        pointerEvents="none"
+        zIndex={0}
+      />
+
+      <Container maxW="container.lg" zIndex={1} position="relative">
+        <Stack spacing={{ base: 12, md: 16 }}>
+          {/* Üst Hero Bölümü */}
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            align="center"
+            justify="space-between"
+            gap={{ base: 8, md: 12 }}
+          >
+            <Box flex="1" maxW={{ base: "100%", md: "48%" }}>
+              <MovieImage />
+            </Box>
+
+            <VStack
+              flex="1"
+              align={{ base: "center", md: "start" }}
+              spacing={5}
+            >
+              <AboutHeader />
+              <AboutText />
+              <ExploreButton />
+            </VStack>
+          </Flex>
+
+          {/* Orta Misyon & Vizyon Bölümü */}
+          <MissionVision />
+        </Stack>
+      </Container>
     </Box>
   );
 }
