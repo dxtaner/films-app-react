2802053b8079d919d4cec736adca0a9aa439168f
Taner Özer <tanerozer16@gmail.com>
2026-09-23 15:10:07 +0300

    Refactor Home component layout and styling for improved user experience

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Heading, VStack, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import PopularMoviesCarousel from "../Movies/popularMoviesCarousel.js";
 import TopRatedMovies from "../Movies/TopRatedMovies.js";
@@ -12,12 +12,28 @@ const Home = () => {
   };
 
   return (
-    <Box>
-      <Box mb={8}>
+    <Box bg="gray.950" color="white" minH="100vh">
+      {/* Popüler Filmler - Tam Genişlikte Carousel */}
+      <Box mb={12} position="relative" zIndex={1}>
         <PopularMoviesCarousel />
       </Box>
 
-      <TopRatedMovies handleDetails={showDetails} />
+      {/* Ana İçerik Alanı */}
+      <Container maxW="container.xl" pb={16}>
+        <VStack spacing={12} align="stretch">
+          <Box>
+            <Flex align="center" gap={3} mb={6}>
+              <Box h="10" w="2" bg="red.500" borderRadius="full" />
+              <Heading as="h2" size="xl">
+                En Yüksek Puan Alan Filmler
+              </Heading>
+            </Flex>
+
+            {/* TopRatedMovies bileşeninin içindeki grid'i bu Container'a uygun hale getirdiğini varsayıyorum */}
+            <TopRatedMovies handleDetails={showDetails} />
+          </Box>
+        </VStack>
+      </Container>
     </Box>
   );
 };
