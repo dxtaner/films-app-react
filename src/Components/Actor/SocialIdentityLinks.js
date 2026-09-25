9de8074e900b63fb29ee2b6c42d61271a72efa40
Taner Özer <tanerozer16@gmail.com>
2026-09-25 10:57:08 +0300

    Refactor SocialIdentityLinks component for improved layout, styling, and enhanced user interaction

--- src/Components/Actor/SocialIdentityLinks.js
+++ src/Components/Actor/SocialIdentityLinks.js
@@ -7,7 +7,7 @@ import {
   FaTiktok,
   FaTwitter,
   FaWikipediaW,
-  FaYoutubeSquare,
+  FaYoutube,
 } from "react-icons/fa";
 
 const SocialIdentityLinks = ({ externalIds }) => {
@@ -17,88 +17,82 @@ const SocialIdentityLinks = ({ externalIds }) => {
       icon: FaImdb,
       id: "imdb_id",
       url: `https://www.imdb.com/name/${externalIds?.imdb_id}`,
+      color: "#F5C518",
     },
     {
       name: "WikiData",
       icon: FaWikipediaW,
       id: "wikidata_id",
       url: `https://www.wikidata.org/wiki/${externalIds?.wikidata_id}`,
+      color: "#FFFFFF",
     },
     {
       name: "Facebook",
       icon: FaFacebook,
       id: "facebook_id",
       url: `https://www.facebook.com/${externalIds?.facebook_id}`,
+      color: "#1877F2",
     },
     {
       name: "Twitter",
       icon: FaTwitter,
       id: "twitter_id",
       url: `https://twitter.com/${externalIds?.twitter_id}`,
+      color: "#1DA1F2",
     },
     {
       name: "Instagram",
       icon: FaInstagram,
       id: "instagram_id",
       url: `https://www.instagram.com/${externalIds?.instagram_id}`,
+      color: "#E4405F",
     },
     {
       name: "TikTok",
       icon: FaTiktok,
       id: "tiktok_id",
-      url: `https://www.tiktok.com/${externalIds?.tiktok_id}`,
+      url: `https://www.tiktok.com/@${externalIds?.tiktok_id}`,
+      color: "#FE2C55",
     },
     {
       name: "YouTube",
-      icon: FaYoutubeSquare,
+      icon: FaYoutube,
       id: "youtube_id",
       url: `https://www.youtube.com/${externalIds?.youtube_id}`,
+      color: "#FF0000",
     },
   ];
 
   const hasSocialLinks = socialLinks.some(
-    (link) => externalIds && externalIds[link.id]
+    (link) => externalIds && externalIds[link.id],
   );
 
+  if (!hasSocialLinks) {
+    return (
+      <Text fontSize="xs" color="gray.500">
+        Sosyal medya hesabı bulunamadı.
+      </Text>
+    );
+  }
+
   return (
-    <Flex
-      as="section"
-      alignItems="center"
-      p={2}
-      mt={4}
-      wrap={"wrap"}
-      bg={hasSocialLinks ? "gray.50" : "transparent"}
-      mb={2}
-      borderRadius={hasSocialLinks ? "md" : "none"}>
-      {hasSocialLinks ? (
-        <>
-          {socialLinks.map(
-            (link, index) =>
-              externalIds[link.id] && (
-                <Tooltip
-                  key={index}
-                  label={link.name}
-                  aria-label={link.name}
-                  placement="top">
-                  <Link
-                    key={index}
-                    href={link.url}
-                    isExternal
-                    textDecoration="none"
-                    fontSize="xl"
-                    color="gray.600"
-                    _hover={{ color: "blue.500" }}
-                    mx={2}>
-                    <Icon as={link.icon} boxSize={8} />
-                  </Link>
-                </Tooltip>
-              )
-          )}
-        </>
-      ) : (
-        <Text fontSize="lg" color="gray.600">
-          Sosyal kimlik bilgileri bulunamadı.
-        </Text>
+    <Flex align="center" gap={3} flexWrap="wrap">
+      {socialLinks.map(
+        (link) =>
+          externalIds?.[link.id] && (
+            <Tooltip key={link.id} label={link.name} hasArrow bg="gray.900">
+              <Link
+                href={link.url}
+                isExternal
+                fontSize="xl"
+                color="gray.400"
+                _hover={{ color: link.color, transform: "scale(1.15)" }}
+                transition="all 0.2s"
+              >
+                <Icon as={link.icon} boxSize={6} />
+              </Link>
+            </Tooltip>
+          ),
       )}
     </Flex>
   );
